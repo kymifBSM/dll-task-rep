92948d2996fe4d9b3cecf417feff405b50fa6388
kymifBSM <sandmanfin@gmail.com>
2023-04-16 11:45:45 +0700

    Math dll func

---
+++ MathApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MathFunc;
+
+namespace MathApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = new double[10];
+
+            Console.WriteLine("Пишите по одному значению: ");
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine($"Значение {i+1}: ");
+                nums[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            double sum, product;
+            Class1.SumProductFunc(nums, out sum, out product);
+            Console.WriteLine("Сумма = {0}", sum);
+            Console.WriteLine("Произведение = {0}", product);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathFunc/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFunc
+{
+    public class Class1
+    {
+        public static void SumProductFunc(double[] nums, out double sum, out double product)
+        {
+            sum = 0;
+            product = 1;
+
+            foreach (double num in nums)
+            {
+                sum += num;
+                product *= num;
+            }
+        }
+    }
+}
